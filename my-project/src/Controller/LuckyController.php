7868317fc67635a812ba2b45f96d4bba13cb02d1
Lucas <lucasbrodersen@gmail.com>
2023-07-08 20:18:03 +0100

    sidebar is on

--- my-project/src/Controller/LuckyController.php
+++ my-project/src/Controller/LuckyController.php
@@ -2,16 +2,21 @@
 // src/Controller/LuckyController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
-class LuckyController
+class LuckyController extends AbstractController
 {
     public function number(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('index/index.html.twig', [
+            'number' => $number
+        ]);
+
+//        return new Response(
+//            '<html><body>Lucky number: '.$number.'</body></html>'
+//        );
     }
 }
